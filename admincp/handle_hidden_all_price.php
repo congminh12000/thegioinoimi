5bfe3ffe1caeb39ed53b156c91ee9b0ef856d30a
PCM <congminh12000@gmail.com>
2017-06-02 22:21:19 +0700

    update

--- admincp/handle_hidden_all_price.php
+++ admincp/handle_hidden_all_price.php
@@ -7,7 +7,7 @@
  */
 
 require_once('../Connections/cnn_hoaly.php');
-mysql_select_db($database_cnn_hoaly, $cnn_hoaly);
+//mysql_select_db($database_cnn_hoaly, $cnn_hoaly);
 
 $arrResp = [
     'isError' => true
@@ -15,9 +15,15 @@ $arrResp = [
 
 if ($_GET) {
     $isCheck = $_GET['isCheck'];
-
+    
     $strQuery = 'UPDATE product SET is_hidden_price = ' . $isCheck;
-    $query = mysql_query($strQuery);
+    
+    $mysqli = new mysqli($hostname_cnn_hoaly, $username_cnn_hoaly, $password_cnn_hoaly, $database_cnn_hoaly);
+    $mysqli->autocommit(false);
+    $mysqli->query($strQuery);
+    $mysqli->commit();
+    
+//    $query = mysql_query($strQuery);
 
     $arrResp = [
         'isError' => false
